d2a20efffb8f7afb0facaa810dcb05892baec8d9
hdlee <hdlee@plateer.com>
2022-03-24 17:27:20 +0900

    환경오류

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -7,16 +7,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          아나스타 앱 고도화
+          Edit <code>src/App.js</code> and save to reload.
         </p>
-        {/*<a*/}
-        {/*  className="App-link"*/}
-        {/*  href="https://reactjs.org"*/}
-        {/*  target="_blank"*/}
-        {/*  rel="noopener noreferrer"*/}
-        {/*>*/}
-        {/*  Learn React*/}
-        {/*</a>*/}
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
